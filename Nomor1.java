75a9da6b6a6bec93b733c9e1e7681e8585040762
Rafiabiassyarif <rafiabiassyarif@gmail.com>
2024-12-12 13:14:53 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
-public class Nomor1 {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+public class Assesment2 {
+    public static double hitungLuas(double alas, double tinggi) {
+        return (alas * tinggi) / 2;
     }
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan alas segitiga: ");
+        double alas = scanner.nextDouble();
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        double tinggi = scanner.nextDouble();
+
+        double luas = hitungLuas(alas, tinggi);
+
+        System.out.println("Luas segitiga adalah: " + luas);
+    }
 }
